26ab1c67dad582248a3b07cbd712a13a7873b051
carlosaquino <39394611+carlosaquinoperez@users.noreply.github.com>
2026-04-09 11:32:37 +0000

    feat: automate athena table creation using terraform

--- terraform/catalog.tf
+++ terraform/catalog.tf
@@ -4,3 +4,19 @@
 resource "aws_glue_catalog_database" "ecommerce_catalog" {
   name = "ecommerce_db"
 }
+
+# 2. The Delta Table Definition
+resource "aws_glue_catalog_table" "silver_orders" {
+  name          = "silver_orders"
+  database_name = aws_glue_catalog_database.ecommerce_catalog.name
+
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    "table_type" = "DELTA"
+  }
+
+  storage_descriptor {
+    location = "s3://${aws_s3_bucket.silver.bucket}/cleaned/orders/"
+  }
+}
